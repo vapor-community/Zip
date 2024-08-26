361ed1de392629388540820e9c154002c1745f7a
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-08-26 11:54:59 +0200

    Fix API breakage and C files compilation

--- Sources/Zip/QuickZip.swift
+++ Sources/Zip/QuickZip.swift
@@ -17,6 +17,21 @@ extension Zip {
         .documentDirectory
         #endif
     }
+
+    /**
+     Quickly unzips a file.
+     
+     Unzips to a new folder inside the app's documents folder with the zip file's name.
+     
+     - Parameter path: Path of zipped file.
+     
+     - Throws: `ZipError.unzipFail` if unzipping fails or `ZipError.fileNotFound` if file is not found.
+     
+     - Returns: `URL` of the destination folder.
+     */
+    public class func quickUnzipFile(_ path: URL) throws -> URL {
+        return try quickUnzipFile(path, progress: nil)
+    }
     
     /**
      Quickly unzips a file.
@@ -41,6 +56,23 @@ extension Zip {
         let destinationUrl = documentsUrl.appendingPathComponent(directoryName, isDirectory: true)
         try self.unzipFile(path, destination: destinationUrl, overwrite: true, password: nil, progress: progress)
         return destinationUrl
+    }
+
+    /**
+     Quickly zips files.
+     
+     - Parameters:
+       - paths: Array of `URL` filepaths.
+       - fileName: File name for the resulting zip file.
+     
+     - Throws: `ZipError.zipFail` if zipping fails.
+     
+     > Note: Supports implicit progress composition.
+     
+     - Returns: `URL` of the destination folder.
+     */
+    public class func quickZipFiles(_ paths: [URL], fileName: String) throws -> URL {
+        return try quickZipFiles(paths, fileName: fileName, progress: nil)
     }
     
     /**
